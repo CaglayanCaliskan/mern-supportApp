87b1abf87f5eec4dbe7ea9d71de06d3e852ecfab
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-02-22 15:45:50 +0300

    basic express setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('hello Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hello canim dedik' });
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on --> http://localhost:${PORT}`)
+);
