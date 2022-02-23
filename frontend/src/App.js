f39763a63b3d1582b0b119e7c9b1be55948b415a
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-02-23 13:23:16 +0300

    Header and initial Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+//Pages
+import Header from './components/Header';
 function App() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
