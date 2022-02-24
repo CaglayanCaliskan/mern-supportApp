4a86e1c176253f1ace6852aac2886d9ffc055a29
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-02-24 12:22:32 +0300

    Get and Create Tickets

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -8,7 +8,17 @@ const Ticket = require('../models/ticketModel');
 // @access Private
 
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'getTickets' });
+  //  get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 // @desc   Create new tickets
@@ -16,7 +26,28 @@ const getTickets = asyncHandler(async (req, res) => {
 // @access Private
 
 const createTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'createTickets' });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Please add a product and description');
+  }
+  //  get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = {
