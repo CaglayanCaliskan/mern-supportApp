382f9c63e7a18d712c9f0f140c635f35ff5f1fab
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-02-25 14:32:53 +0300

    Close Ticket Functionality

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
-import { getTicket, reset } from '../features/tickets/ticketSlice';
+import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
 
 function Ticket() {
   const { ticket, isLoading, isSuccess, isError, message } = useSelector(
@@ -12,6 +12,7 @@ function Ticket() {
   );
 
   const params = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { ticketId } = useParams();
 
@@ -20,12 +21,27 @@ function Ticket() {
       toast.error(message);
     }
     dispatch(getTicket(ticketId));
+    // eslint-disavle-next-line
   }, [isError, message, ticketId]);
+
+  // Close Ticket
+
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId));
+    toast.success('Ticket Closed');
+    navigate('/tickets');
+  };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <h3>Something Went Wrong</h3>;
+  }
 
   return (
     <div className='ticket-page'>
-      ticketssss
-      {/* <header className='ticket-header'>
+      <header className='ticket-header'>
         <BackButton url='/tickets' />
         <h2>
           Ticket ID: {ticket._id}
@@ -33,7 +49,23 @@ function Ticket() {
             {ticket.status}
           </span>
         </h2>
-      </header> */}
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-EU')}
+        </h3>
+        <h3> Product: {ticket.product}</h3>
+        <hr />
+        <div className='ticket-desc'>
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+
+      {ticket.status !== 'closed' && (
+        <button onClick={onTicketClose} className='btn btn-block btn-danger'>
+          {' '}
+          Close Ticket
+        </button>
+      )}
     </div>
   );
 }
